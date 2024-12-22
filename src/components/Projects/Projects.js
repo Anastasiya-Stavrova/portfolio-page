1254f461a7a7992a03e819186025a93894948ecc
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2024-12-22 12:45:47 +0700

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,35 +1,53 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import ProjectCard from "../ProjectCard/ProjectCard";
 import "./Projects.css";
+import ColorSharp from "./../../assets/img/color-sharp2.png";
+import { projects } from "../../consts/projects";
 
 const Projects = () => {
-  const projects = [
-    {
-      title: "",
-      description: "",
-      imgUrl: "",
-    },
-    {
-      title: "",
-      description: "",
-      imgUrl: "",
-    },
-    {
-      title: "",
-      description: "",
-      imgUrl: "",
-    },
-    {
-      title: "",
-      description: "",
-      imgUrl: "",
-    },
-    {
-      title: "",
-      description: "",
-      imgUrl: "",
-    },
-  ];
-
-  return <section> </section>;
+  return (
+    <section className="projects" id="projects">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>My small study projects.</p>
+            <Tab.Container id="projects-tabs" defaultActiveKey={"first"}>
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey={"first"}>Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={"second"}>Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={"third"}>Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey={"first"}>
+                  <Row>
+                    {projects.map((project, index) => {
+                      return (
+                        <ProjectCard key={index} {...project}></ProjectCard>
+                      );
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey={"second"}>*****</Tab.Pane>
+                <Tab.Pane eventKey={"third"}>*****</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="bg-img-right" src={ColorSharp} alt="bg-img" />
+    </section>
+  );
 };
 
 export default Projects;
---
+++ src/consts/projects.js
@@ -0,0 +1,25 @@
+import projectImg1 from "../assets/img/project-img1.png";
+import projectImg2 from "../assets/img/project-img2.png";
+import projectImg3 from "../assets/img/project-img3.png";
+
+const projects = [
+  {
+    title: "Algorithm Website",
+    description:
+      "Site with various algorithms : A*, clustering," +
+      " ant algorithm, genetic algorithm, neural network, decision tree.",
+    imgUrl: projectImg1,
+  },
+  {
+    title: "To-Do List",
+    description: "Simple To-Do List with backend.",
+    imgUrl: projectImg2,
+  },
+  {
+    title: "Blog",
+    description: "A blog website for reviewing posts and chatting.",
+    imgUrl: projectImg3,
+  },
+];
+
+export { projects };
